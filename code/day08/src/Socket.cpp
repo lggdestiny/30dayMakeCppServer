554a6bc31e937947dededfc0579b517f53be96e5
lggdestiny <110278709+lggdestiny@users.noreply.github.com>
2023-05-15 15:36:26 +0800

    Update Socket.cpp

--- code/day08/src/Socket.cpp
+++ code/day08/src/Socket.cpp
@@ -5,11 +5,13 @@
 #include <fcntl.h>
 #include <sys/socket.h>
 #include <string.h>
-
-Socket::Socket() : fd(-1){
+//生成监听套接字
+Socket::Socket() : fd(-1)
+{
     fd = socket(AF_INET, SOCK_STREAM, 0);
     errif(fd == -1, "socket create error");
 }
+//检查是否为有效套接字
 Socket::Socket(int _fd) : fd(_fd){
     errif(fd == -1, "socket create error");
 }
@@ -20,21 +22,21 @@ Socket::~Socket(){
         fd = -1;
     }
 }
-
+//把服务器地址和监听套接字绑定
 void Socket::bind(InetAddress *_addr){
     struct sockaddr_in addr = _addr->getAddr();
     socklen_t addr_len = _addr->getAddr_len();
     errif(::bind(fd, (sockaddr*)&addr, addr_len) == -1, "socket bind error");
     _addr->setInetAddr(addr, addr_len);
 }
-
+//监听
 void Socket::listen(){
     errif(::listen(fd, SOMAXCONN) == -1, "socket listen error");
 }
 void Socket::setnonblocking(){
     fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
 }
-
+//接收客户端连接，并且返回客户端socketfd
 int Socket::accept(InetAddress *_addr){
     struct sockaddr_in addr;
     socklen_t addr_len = sizeof(addr);
